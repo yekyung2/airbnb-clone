170e35b2e47eb782e241cb3910078a145d7475df
yekyunglee <skybluee2014@gmail.com>
2020-11-02 21:51:31 +0900

    add avatar, gender in user models

--- users/models.py
+++ users/models.py
@@ -4,5 +4,21 @@ from django.db import models
 # Create your models here.
 
 
+# default="" or null=True
 class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
     bio = models.TextField(default="")

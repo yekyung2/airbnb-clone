53d0064c9c86fe110ea71fbb7378aa3e10656fd3
yekyunglee <skybluee2014@gmail.com>
2020-11-02 22:43:28 +0900

    add new lists in custom user Model

--- users/models.py
+++ users/models.py
@@ -19,6 +19,29 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
-    bio = models.TextField(default="")
+    LANGUAGES_ENGLISH = "en"
+    LANGUAGES_KOREAN = "kr"
+
+    LANGUAGES_CHOICES = [
+        (LANGUAGES_ENGLISH, 'English'),
+        (LANGUAGES_KOREAN, 'Korean'),
+    ]
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = [
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW")
+    ]
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES,
+                              max_length=10, null=True, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGES_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
